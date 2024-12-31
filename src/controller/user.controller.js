4935861600c6f311393b9e0d4a6d18676211f441
jhomel43769 <jhomelmedina2@gmail.com>
2024-12-31 15:35:58 +0800

    metodo get creado

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -1,1 +1,11 @@
+import { prisma } from '../../db.js';
 
+export const getUsers = async(req, res) => {
+    try {
+        const users = await prisma.users.findMany();
+
+        res.status(200).json(users);
+    } catch (error) {
+        res.status(400).json({ error: 'Error en el metodo GET', messagge: error.message });
+    }
+};
